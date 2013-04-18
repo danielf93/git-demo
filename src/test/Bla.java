11c3ea8a5454830b10978739ef1906d40037bb14
danielf <dani.f@live.de>
2013-04-18 21:31:32 +0200

    more changes

--- src/test/Bla.java
+++ src/test/Bla.java
@@ -2,7 +2,11 @@ package test;
 
 public class Bla {
 	
+	public static String getMessage() {
+		return "Hello git";
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println(getMessage());
 	}
 }
